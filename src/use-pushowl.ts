7a7b5d0b979d52510c0011e51861071042049d59
Rafi <rafi993@protonmail.com>
2021-10-12 15:44:53 +0530

    Adding has loaded flag

--- src/use-pushowl.ts
+++ src/use-pushowl.ts
@@ -1,15 +1,13 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useCartState } from 'frontend-checkout'
 import { useCustomerState } from 'frontend-customer'
 import { StorePlatformDomain, Pushowl } from './types'
 
 export const usePushowl = (subdomain: string) => {
     const cart = useCartState()
+    const [hasLoaded, setHasLoaded] = useState(false)
     const { id: customerId } = useCustomerState()
     const cartId = cart.id
-
-    // we do use window.location.href to check it is in the product page
-    // const isProductPage = /\/products/.exec(window.location.href)
 
     const cartItems = cart.items
 
@@ -17,6 +15,47 @@ export const usePushowl = (subdomain: string) => {
     useEffect(() => {
         if (document.querySelector('[data-script="pushowl"]')) {
             return
+        }
+
+        const injectScript = (subdomain: StorePlatformDomain | null) => {
+            if (!window.pushowl) {
+                const pushowl: Pushowl = {
+                    queue: [],
+                    subdomain,
+                    trigger: (
+                        taskName: string,
+                        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                        taskData: any,
+                    ) => {
+                        return new Promise((resolve, reject) => {
+                            window.pushowl.queue.push({
+                                taskName,
+                                taskData,
+                                promise: { resolve, reject },
+                            })
+                        })
+                    },
+                    init: () => {
+                        if (subdomain === null) {
+                            return
+                        }
+
+                        const script = document.createElement('script')
+                        script.type = 'text/javascript'
+                        script.async = true
+                        script.src = `https://cdn.pushowl.com/sdks/pushowl-sdk.js?subdomain=${subdomain}&environment=production&shop=${subdomain}.myshopify.com`
+                        document.body.append(script)
+                        onload = () => {
+                            setHasLoaded(true)
+                        }
+                        return script
+                    },
+                }
+
+                window.pushowl = pushowl
+            }
+
+            window.pushowl.init()
         }
 
         injectScript(subdomain)
@@ -27,11 +66,6 @@ export const usePushowl = (subdomain: string) => {
         window.pushowl.trigger('setCustomerId', customerId)
     }, [customerId])
 
-    // // sync product when in product page
-    // useEffect(() => {
-    //     window.pushowl.trigger('syncProductView', { productId: 89789578956 })
-    // }, [isProductPage])
-
     // sync cart when its items change
     useEffect(() => {
         if (cartId !== null) {
@@ -41,9 +75,6 @@ export const usePushowl = (subdomain: string) => {
                 quantity: item.quantity,
             }))
 
-            // eslint-disable-next-line no-console
-            console.log('items', items)
-
             window.pushowl.trigger('syncCart', {
                 items,
                 checkoutToken: atob(cartId).split('/').pop().split('?key=')[0],
@@ -51,49 +82,7 @@ export const usePushowl = (subdomain: string) => {
         }
     }, [cartId, cartItems])
 
-    const showWidget = (payload: any) => {
-        return window.pushowl.trigger('showWidget', payload)
-    }
-
     return {
-        showWidget,
+        hasLoaded,
     }
-}
-
-const injectScript = (subdomain: StorePlatformDomain | null) => {
-    if (!window.pushowl) {
-        const pushowl: Pushowl = {
-            queue: [],
-            subdomain,
-            trigger: (
-                taskName: string,
-                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                taskData: any,
-            ) => {
-                return new Promise((resolve, reject) => {
-                    window.pushowl.queue.push({
-                        taskName,
-                        taskData,
-                        promise: { resolve, reject },
-                    })
-                })
-            },
-            init: () => {
-                if (subdomain === null) {
-                    return
-                }
-
-                const script = document.createElement('script')
-                script.type = 'text/javascript'
-                script.async = true
-                script.src = `https://cdn.pushowl.com/sdks/pushowl-sdk.js?subdomain=${subdomain}&environment=production&shop=${subdomain}.myshopify.com`
-                document.body.append(script)
-                return script
-            },
-        }
-
-        window.pushowl = pushowl
-    }
-
-    window.pushowl.init()
 }
