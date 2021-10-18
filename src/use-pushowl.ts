0dfe6c0f5e206247bf738e380263787ae1014818
Rafi <rafi993@protonmail.com>
2021-10-18 20:27:01 +0530

    Fixing linting errors

--- src/use-pushowl.ts
+++ src/use-pushowl.ts
@@ -68,23 +68,51 @@ export const usePushowl = (subdomain: string) => {
     // Syncing customer id
     useEffect(() => {
         if (customerId !== null && canSync) {
-            const decodedCustomerId = parseInt(atob(`${customerId}`).split('/').pop() || '', 10)
-            window.pushowl.trigger('setCustomerId', decodedCustomerId)
+            const decodedCustomerId = atob(`${customerId}`).split('/').pop()
+            if (decodedCustomerId !== undefined) {
+                window.pushowl.trigger('setCustomerId', parseInt(decodedCustomerId))
+            }
         }
     }, [customerId, canSync])
 
     // sync cart when its items change
     useEffect(() => {
         if (cartId !== null && canSync) {
-            const items = cartItems.map((item) => ({
-                variantId: atob(item.variant.id).split('/').pop(),
-                productId: atob(item.id).split('/').pop().split('?checkout=')[0],
-                quantity: item.quantity,
-            }))
+            const items = cartItems.map((item) => {
+                const productWithCheckout = atob(item.id).split('/').pop()
+
+                let productId = null
+
+                if (productWithCheckout !== undefined && productWithCheckout.length > 0) {
+                    productId = parseInt(productWithCheckout.split('?checkout=')[0], 10)
+                }
+
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment, @typescript-eslint/ban-ts-ignore
+                // @ts-ignore
+                const variantIdStr = atob(item.variant.id).split('/').pop()
+                let variantId = null
+
+                if (variantIdStr !== undefined) {
+                    variantId = parseInt(variantIdStr, 10)
+                }
+
+                return {
+                    variantId,
+                    productId,
+                    quantity: item.quantity,
+                }
+            })
+
+            let checkoutToken = null
+            const checkoutTokenWithKey = atob(cartId).split('/').pop()
+
+            if (checkoutTokenWithKey !== undefined) {
+                checkoutToken = checkoutTokenWithKey.split('?key=')[0]
+            }
 
             window.pushowl.trigger('syncCart', {
                 items,
-                checkoutToken: atob(cartId).split('/').pop().split('?key=')[0],
+                checkoutToken,
             })
         }
     }, [canSync, cartId, cartItems])
